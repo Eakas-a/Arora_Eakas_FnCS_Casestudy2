b89984437e420be4c93c0ca76731d1636aa3a9a3
Eakas-a <aroraeakas14@gmail.com>
2026-08-26 18:25:47 +0530

    3.1

--- java-assignment/src/main/java/com/fulfilment/application/monolith/fulfillment/domain/models/ProductFulfillmentAssociation.java
+++ java-assignment/src/main/java/com/fulfilment/application/monolith/fulfillment/domain/models/ProductFulfillmentAssociation.java
@@ -1,33 +1,19 @@
 package com.fulfilment.application.monolith.fulfillment.domain.models;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import com.fulfilment.application.monolith.products.Product;
 import com.fulfilment.application.monolith.stores.Store;
 import com.fulfilment.application.monolith.warehouses.adapters.database.DbWarehouse;
 
-/**
- * Associates a {@link Product} with a {@link Store} and the {@link DbWarehouse} that acts as its
- * fulfillment unit for that store - i.e. "this warehouse is one of the places this product is
- * shipped from, in order to stock this particular store".
- *
- * <p>A (product, store, warehouse) triple is unique: the same warehouse can't be registered twice
- * as a fulfillment unit for the same product/store pair. Quantity represents how many units of
- * the product this particular warehouse is committed to supplying to this store.
- *
- * <p>This is a framework-free domain model - persistence concerns live in the {@code adapters}
- * package, behind the {@link com.fulfilment.application.monolith.fulfillment.domain.ports.FulfillmentAssociationRepository}
- * port.
- */
 @Entity
 public class ProductFulfillmentAssociation {
 
+  @Id
   public Long id;
 
   public Product product;
-
   public Store store;
-
   public DbWarehouse warehouse;
-
   public int quantity;
 
   public ProductFulfillmentAssociation() {}
